c4ee5072522c1ea4c6075a9d785357247db837af
airizarryOSU <67487429+airizarryOSU@users.noreply.github.com>
2020-08-02 23:56:32 -0400

    Update jailbreak.py

--- irizarra/jailbreak.py
+++ irizarra/jailbreak.py
@@ -3,6 +3,6 @@ import requests
 
 url = "https://jailbreakbrewing.com/"
 html = requests.get(url)
-soup = BeautifulSoup(html.text, "html5lib")
+soup = BeautifulSoup(html.text, "html.parser")
 beerTable = soup.select("table",class_ = "table table-hover table-condensed table-striped volumes")[0]
 print(beerTable)
